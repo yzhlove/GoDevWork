491befeae09a7ace31d36fc1d4ae4ed2ab13adbd
Rain <yostar@huajiangxiashudeMacMini.local>
2020-01-13 15:09:01 +0800

    rc4

---
+++ Chats/pwdgraph/chat01/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"crypto/rc4"
+	"fmt"
+)
+
+//RC4 加解密
+
+var key = []byte("abcdefghijklmnopqrstuvwxyz")
+
+func main() {
+	
+	result := RC4Encrypt([]byte("yuzihan"))
+	fmt.Printf("%x \n", result)
+
+	fmt.Println(string(RC4Decrypt(result)))
+
+}
+
+func RC4Encrypt(value []byte) []byte {
+	if cipher, err := rc4.NewCipher(key); err != nil {
+		panic(err)
+	} else {
+		cipher.XORKeyStream(value, value)
+	}
+	return value
+}
+
+func RC4Decrypt(source []byte) []byte {
+	if cipher, err := rc4.NewCipher(key); err != nil {
+		panic(err)
+	} else {
+		cipher.XORKeyStream(source, source)
+	}
+	return source
+}
