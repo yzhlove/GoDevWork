4ee7103c63bf7895c112494fe6c9067a6a280e6e
Rain <yostar@huajiangxiashudeMacMini.local>
2020-04-24 18:45:12 +0800

    lfu

--- CacheBase/day1-base4/main.go
+++ CacheBase/day1-base4/main.go
@@ -1,9 +1,125 @@
 package main
 
+import "container/list"
+
 //////////////////////////////////////////////
 // LFU
 //////////////////////////////////////////////
 
 func main() {
-	
+
+	cacheLFU := New(3)
+	cacheLFU.Set("one", 1)
+	cacheLFU.Set("two", 2)
+
+}
+
+type CacheItem struct {
+	key             string
+	value           interface{}
+	frequencyParent *list.Element
+}
+
+type FrequencyItem struct {
+	entries map[*CacheItem]byte
+	freq    int
+}
+
+type Cache struct {
+	bykey    map[string]*CacheItem
+	freqs    *list.List
+	capacity int
+	size     int
+}
+
+func New(max int) *Cache {
+	return &Cache{
+		bykey:    make(map[string]*CacheItem),
+		freqs:    list.New(),
+		size:     0,
+		capacity: max,
+	}
+}
+
+func (cache *Cache) Set(key string, value interface{}) {
+	if item, ok := cache.bykey[key]; ok {
+		item.value = value
+	} else {
+		item = &CacheItem{
+			key:   key,
+			value: value,
+		}
+		cache.bykey[key] = item
+		cache.size++
+		if cache.atCapacity() {
+			cache.Evict(10)
+		}
+		cache.increment(item)
+	}
+}
+
+func (cache *Cache) Get(key string) interface{} {
+	if e, ok := cache.bykey[key]; ok {
+		cache.increment(e)
+		return e.value
+	}
+	return nil
+}
+
+func (cache *Cache) increment(item *CacheItem) {
+	currentFrequency := item.frequencyParent
+	var nextFrequencyAmount int
+	var nextFrequency *list.Element
+
+	if currentFrequency == nil {
+		nextFrequencyAmount = 1
+		nextFrequency = cache.freqs.Front()
+	} else {
+		nextFrequencyAmount = currentFrequency.Value.(*FrequencyItem).freq + 1
+		nextFrequency = currentFrequency.Next()
+	}
+
+	if nextFrequency == nil || nextFrequency.Value.(*FrequencyItem).freq != nextFrequencyAmount {
+		newFrequencyItem := &FrequencyItem{
+			freq:    nextFrequencyAmount,
+			entries: make(map[*CacheItem]byte),
+		}
+		if currentFrequency == nil {
+			nextFrequency = cache.freqs.PushFront(newFrequencyItem)
+		} else {
+			nextFrequency = cache.freqs.InsertAfter(newFrequencyItem, currentFrequency)
+		}
+	}
+	item.frequencyParent = nextFrequency
+	nextFrequency.Value.(*FrequencyItem).entries[item] = 1
+	if currentFrequency != nil {
+		cache.Remove(currentFrequency, item)
+	}
+}
+
+func (cache *Cache) Remove(listItem *list.Element, item *CacheItem) {
+	frequencyItem := listItem.Value.(*FrequencyItem)
+	delete(frequencyItem.entries, item)
+	if len(frequencyItem.entries) == 0 {
+		cache.freqs.Remove(listItem)
+	}
+}
+
+func (cache *Cache) Evict(count int) {
+	for i := 0; i < count; {
+		if item := cache.freqs.Front(); item != nil {
+			for entity := range item.Value.(*FrequencyItem).entries {
+				if i < count {
+					delete(cache.bykey, entity.key)
+					cache.Remove(item, entity)
+					cache.size--
+					i++
+				}
+			}
+		}
+	}
+}
+
+func (cache *Cache) atCapacity() bool {
+	return cache.size >= cache.capacity
 }
